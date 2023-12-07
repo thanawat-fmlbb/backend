a1d225062d932254ead9296cb958f51d2b64d282
thanawinboon <pokepat27@gmail.com>
2023-12-07 22:05:11 +0700

    added more setup for db

--- src/api/control_panel.py
+++ src/api/control_panel.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, Request
+
+from src.models.db import create_db_and_tables
+from src.models.thing_model import thing_setup
 from src.models.user_model import user_setup
 from src.utils.celery import get_celery_app, ChannelEnum
 
@@ -11,7 +14,9 @@ async def control_panel(request: Request):
 
 @router.get("/setup")
 async def service_setup():
+    create_db_and_tables()
     user_setup()
+    thing_setup()
 
     # send message to create order service
     celery = get_celery_app(ChannelEnum.PAYMENT.value)
--- src/models/thing_model.py
+++ src/models/thing_model.py
@@ -14,3 +14,29 @@ def get_price(item_id: int):
         statement = select(Thing).where(Thing.id == item_id)
         thing = session.exec(statement).one()
         return thing.price
+
+def thing_setup():
+    with Session(get_engine()) as session:
+        try:
+            
+            item1 = session.get(Thing, 1)
+            if item1 is None:
+                item1 = Thing(id=1, name="thing", price=10)
+                session.add(item1)
+                session.commit()
+
+            item2 = session.get(Thing, 2)
+            if item2 is None:
+                item2 = Thing(id=2, name="empty_thing", price=20)
+                session.add(item2)
+                session.commit()
+
+            item3 = session.get(Thing, 3)
+            if item3 is None:
+                item3 = Thing(id=3, name="another_thing", price=10)
+                session.add(item3)
+                session.commit()
+            return True
+        except Exception as e:
+            print(str(e))
+            return False
